49b3cbb398ccf87567b784264e8061e23e81074a
fr0sty <an0n102968@riseup.net>
2013-08-08 14:48:40 -0400

    Update correct.pl
    Fixed clumsy EOF

--- correct.pl
+++ correct.pl
@@ -3,18 +3,18 @@ $from = $ARGV[1];
 $to = $ARGV[2];
 
 if ($richpoor ne "rich" && $richpoor ne "poor") {
-    die "first argument must be  either 'rich' or 'poor'";
+    die "First argument must be  either 'rich' or 'poor'";
 }
 
 
 
 #Ok, this script is pretty embarassing and very quick and dirty but I just want to get it done
-system '(
+system
     echo \'<?xml version="1.0" encoding="utf-8"?>'.
                             
                             
-                            #Those are hosted on runn1ng's github page
-'    
+#Those are hosted on runn1ng's github page
+'
 <!DOCTYPE archive PUBLIC "piratebay'.$richpoor.'" "http://runn1ng.github.com/'.$richpoor.'.dtd">
 <archive>\'; 
 cat '.$from.'; 
